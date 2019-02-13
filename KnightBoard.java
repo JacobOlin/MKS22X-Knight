83217f884ef864a43cec5f00038f3d31e2fb6298
Jacob Olin <jolin00@stuy.edu>
2019-02-13 11:13:05 -0500

    The constructor now throws in IllegalArgumentException when either parameters is less than or equal to 0

--- KnightBoard.java
+++ KnightBoard.java
@@ -2,6 +2,9 @@ public class KnightBoard {
   private int[][] board;
 
   public KnightBoard(int startingRows,int startingCols) {
+    if (startingRows <= 0 || startingCols <= 0) {
+      throw new IllegalArgumentException();
+    }
     board = new int[startingRows][startingCols];
     for (int i = 0;i < board.length;i += 1) {
       for (int j = 0;j < board[i].length;j += 1) {
@@ -40,7 +43,7 @@ public class KnightBoard {
     if (startingRow < 0 || startingRow >= board.length || startingCol < 0 || startingCol >= board[0].length) {
       throw new IllegalArgumentException();
     }
-    return helper(startingRow,startingCol,0);
+    return helper(startingRow,startingCol,1);
   }
 
   public boolean helper(int row, int col,int move) {
