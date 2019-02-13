203870201d35d272262fadf30f164291d0a1e533
JacobOlin <jolin00@stuy.edu>
2019-02-12 22:48:55 -0500

    Added the shell for toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -9,7 +9,11 @@ public class KnightBoard {
       }
     }
   }
-  
+
+  public String toString() {
+    return "";
+  }
+
   public boolean solve(int startRow,int StartCol) {
     return true;
   }
