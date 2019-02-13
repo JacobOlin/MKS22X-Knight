d3454579cede8203d9df41fb31c4a5252938ab7c
JacobOlin <jolin00@stuy.edu>
2019-02-12 22:48:29 -0500

    Wrote the constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,4 +1,15 @@
 public class KnightBoard {
+  private int[][] board;
+
+  public KnightBoard(int startingRows,int startingCols) {
+    board = new int[startingRows][startingCols];
+    for (int i = 0;i < board.length;i += 1) {
+      for (int j = 0;j < board[i].length;j += 1) {
+        board[i][j] = 0;
+      }
+    }
+  }
+  
   public boolean solve(int startRow,int StartCol) {
     return true;
   }
